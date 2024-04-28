8e9a7eb94de1da194d2b9a332e4a5402f311df2e
Lyciaa <57102194+Mavenless@users.noreply.github.com>
2024-04-28 17:15:07 +0200

    Mettre à jour la barre latérale pour afficher un bouton de réduction au lieu d'un bouton d'expansion

--- frontend/src/lib/Sidebar.svelte
+++ frontend/src/lib/Sidebar.svelte
@@ -11,7 +11,7 @@
 <div class="w-64 bg-panel p-5 min-h-full rounded-3xl ml-5 my-5" transition:slide>
     <div class="flex items-center space-x-2 mb-5">
       <div class="absolute top-4 left-64">
-        <button on:click={() => sidebar = false } out:slide={{ duration: 100 }} ><i class="fas fa-plus text-white text-xl text-right top-64 right-48 bg-panel rounded-full p-1"></i></button>
+        <button on:click={() => sidebar = false } out:slide={{ duration: 100 }} ><i class="fas fa-minus text-white text-xl text-right top-64 right-48 bg-panel rounded-full p-1"></i></button>
       </div>
     <!-- <i class="fas fa-play-circle text-white text-3xl"></i> -->
       <span class="text-white font-bold text-xl">Merdeko</span>
@@ -34,5 +34,5 @@
 </div>
 </div>
 {:else}
-  <button on:click={() => sidebar = true}><i class="fas fa-minus text-white text-xl text-right bg-panel rounded-full p-1 absolute top-10 left-0" out:fade={{ duration: 200 }} in:fade={{ delay: 200, duration: 100 }}></i></button>
+  <button on:click={() => sidebar = true}><i class="fas fa-plus text-white text-xl text-right bg-panel rounded-full p-1 absolute top-10 left-0" out:fade={{ duration: 200 }} in:fade={{ delay: 200, duration: 100 }}></i></button>
 {/if}
